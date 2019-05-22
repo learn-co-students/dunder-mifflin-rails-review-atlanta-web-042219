735a821b899206958ce02b2f48fd8728e9873b93
Preston Elliott <preston.elliott24@gmail.com>
2019-05-22 18:34:43 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,27 @@
 class DogsController < ApplicationController
+    def new
+        @dog = Dog.new
+    end
+
+    def create
+        @dog = Dog.new(dog_params)
+        if @dog.save
+            redirect_to dog_path(@dog)
+        else 
+            render :new
+        end
+    end
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+    private
+    def dog_params
+        params.require(:dog).permit(:name, :breed, :age)
+    end
 end
